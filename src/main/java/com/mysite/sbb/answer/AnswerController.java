fb01b8a97998dc40306dfbae16d9785a096d6ebd
an9383 <an9383@gmail.com>
2024-03-03 11:11:11 +0900

    teacher

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,22 +1,19 @@
 package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.QuestionService;
 import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -44,55 +41,4 @@ public class AnswerController {
 
         return String.format("redirect:/question/detail/%s", id);
     }
-
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/modify/{id}")
-    public String answerModify(@PathVariable("id") Integer id, AnswerForm answerForm, Principal principal) {
-        Answer answer = this.answerService.getAnswer(id);
-        // 로그인한 유저의 작성한 유저에 한해서만 수정가능 하도록 방어코드 작성
-        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-        }
-
-        answerForm.setContent(answer.getContent());
-
-        return "answer_form";
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @PostMapping("/modify/{id}")
-    public String answerModify(
-            @PathVariable("id") Integer id,
-            @Valid AnswerForm answerForm,
-            BindingResult bindingResult,
-            Principal principal) {
-        if (bindingResult.hasErrors()) {
-            return "answer_form";
-        }
-
-        Answer answer = this.answerService.getAnswer(id);
-        // 로그인한 유저의 작성한 유저에 한해서만 수정가능 하도록 방어코드 작성
-        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-        }
-
-        // 실제 게시판 수정로직
-        this.answerService.modify(answer, answerForm.getContent());
-
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/delete/{id}")
-    public String AnswerDelete(@PathVariable("id") Integer id, Principal principal) {
-        Answer answer = this.answerService.getAnswer(id);
-        // 로그인한 유저의 작성한 유저에 한해서만 수정가능 하도록 방어코드 작성
-        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
-        }
-        this.answerService.delete(answer);
-
-        return "redirect:/";
-    }
-
 }
--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -4,8 +4,6 @@ import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
-import com.mysite.sbb.DataNotFoundException;
 
 import java.time.LocalDateTime;
 
@@ -21,23 +19,5 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(siteUser);
         this.answerRepository.save(answer);
-    }
-    public Answer getAnswer(Integer id) {
-        Optional<Answer> answer = this.answerRepository.findById(id);
-        if (answer.isPresent()) {
-            return answer.get();
-        } else {
-            throw new DataNotFoundException("answer not found");
-        }
-    }
-
-    public void modify(Answer answer, String content) {
-        answer.setContent(content);
-        answer.setModifyDate(LocalDateTime.now());
-        this.answerRepository.save(answer);
-    }
-
-    public void delete(Answer answer) {
-        this.answerRepository.delete(answer);
     }
 }
